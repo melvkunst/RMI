719da8f38929d25307eace4e613db9dcfc1936d5
Melissa Viana Kunst <melvkunst@gmail.com>
2024-06-12 20:41:57 -0300

    added more comments

--- src/Servidor.java
+++ src/Servidor.java
@@ -4,12 +4,19 @@ import java.rmi.registry.Registry;
 public class Servidor {
     public static void main(String[] args) {
         try {
+            //Cria uma instância do objeto remoto ContaBancariaImpl
             ContaBancaria conta = new ContaBancariaImpl();
+
+            //Cria e exporta o registro RMI na porta 1099
             Registry registry = LocateRegistry.createRegistry(1099);
+
+            //Liga o nome "ContaBancaria" ao objeto remoto conta no registro RMI
             registry.rebind("ContaBancaria", conta);
+
+            //Mostra quando o servidor está pronto para receber conexões
             System.out.println("Servidor pronto.");
         } catch (Exception e) {
-            System.err.println("Exceção no Servidor: " + e.toString());
+            System.err.println("Excecao no Servidor: " + e.toString());
             e.printStackTrace();
         }
     }
