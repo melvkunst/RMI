14babb3a6695868baad7e3049e7524211685a6f6
Melissa Viana Kunst <melvkunst@gmail.com>
2024-06-12 19:03:32 -0300

    update again

--- src/Servidor.java
+++ src/Servidor.java
@@ -4,9 +4,15 @@ import java.rmi.registry.Registry;
 public class Servidor {
     public static void main(String[] args) {
         try {
+            // Cria uma instância da conta bancária
             ContaBancaria conta = new ContaBancariaImpl();
-           Registry registry = LocateRegistry.createRegistry(1099);
+            
+            // Cria e exporta o registro na porta 1099
+            Registry registry = LocateRegistry.createRegistry(1099);
+            
+            // Registra a conta bancária no registro RMI
             registry.rebind("ContaBancaria", conta);
+            
             System.out.println("Servidor pronto.");
         } catch (Exception e) {
             System.err.println("Exceção no Servidor: " + e.toString());
